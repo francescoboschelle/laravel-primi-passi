a773f9b4b9aae42e1d0cce4def63bab8b0086b4c
francescoboschelle <francibossch@gmail.com>
2025-10-07 18:21:42 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <style>
+        body {
+            background-color: rgba(27, 27, 27, 1);
+            min-height: 100vh;
+            align-items: center;
+            display: flex;
+        }
+
+        .card {
+            background-color: rgba(39, 39, 39, 1);
+        }
+    </style>
+
+    <div class="container">
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="https://api.stafilo.app/upload/blog/laravel.png" class="img-fluid rounded-start" alt="Laravel 12">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body text-white d-flex flex-column justify-content-between h-100">
+                    <div class="card-title">
+                        <h1>Hello Laravel!</h1>
+                        <div class="card-text">
+                            <p>My first laravel project.</p>
+                        </div>
+                    </div>
+                    <small class="text-white">I <span class="text-danger">&hearts;</span> Laravel</small>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
